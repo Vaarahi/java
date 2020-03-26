2a7fcfa0d5c70622d111422138be46e308e01f6b
Vaarahi <61677443+Vaarahi@users.noreply.github.com>
2020-03-26 19:16:10 +0530

    Add files via upload

---
+++ Automation Workspace/java/src/vaa/Modifier1.java
@@ -0,0 +1,12 @@
+package vaa;
+
+public class Modifier1 {
+final int d=10;
+final double pi=3.14;
+	public static void main(String[] args) {
+Modifier1 obj=new Modifier1();
+System.out.println(obj.d);
+System.out.println(obj.pi);
+	}
+
+}
